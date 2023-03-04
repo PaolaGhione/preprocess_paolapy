e3933b7bc7bbae1b2183cfe1f239aa9d27fcbef2
Paola Ghione <ghionep@gmail.com>
2023-03-04 18:04:21 +0100

    update code

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as file:
 
 setuptools.setup(
 	name = 'preprocess_paolapy', #this should be unique
-	version = '0.0.1',
+	version = '0.0.2',
 	author = 'Paola Ghione',
 	author_email = 'info@highmarketing.it',
 	description = 'This is preprocessing package',
